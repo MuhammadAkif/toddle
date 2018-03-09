dce1701c253305c75292475a228e7470d3e04d8a
ahmad.raza <akif.malhi@itboost.com>
2018-03-08 20:16:11 -0800

    Updates

--- src/index.js
+++
@@ -1,8 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './assets/styles/index.css';
-import TodoApp from './components/TodoApp';
-import registerServiceWorker from './utils/registerServiceWorker';
-
-ReactDOM.render(<TodoApp />, document.getElementById('root'));
-registerServiceWorker();
--- src/models/Todo.js
+++
@@ -1,7 +0,0 @@
-export class Todo {
-    constructor(id = 0,text = '',done = false){
-        this.id = id;
-        this.text = text;
-        this.done = done
-    }
-}
